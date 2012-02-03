7b51cadd6ef0267b2f0367d8408667a863faf374
Edwin Morris <edwin.h.morris@gmail.com>
2012-02-03 12:04:31 -0500

    changed hash function so length only gets calculated once - thanks to reddit user random314

--- birdseye.js
+++ birdseye.js
@@ -4,7 +4,7 @@
   String.prototype.hashCode = function() {
     var hash = 0;
     if (this.length == 0) return hash;
-    for (i = 0; i < this.length; i++) {
+    for (var i = 0, l=this.length; i < l; i++) {
       char = this.charCodeAt(i);
       hash = ((hash<<5)-hash)+char;
       hash = hash & hash; // Convert to 32bit integer
